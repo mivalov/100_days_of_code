d0af0893e6c9b1f3caccaf8285afddbadf4b45b2
Miroslav Valov <91765698+mivalov@users.noreply.github.com>
2022-12-13 20:22:27 +0100

    Initial commit

---
+++ 001.py
@@ -0,0 +1,13 @@
+# Day 1: Print into the World
+
+print(
+    'Miroslav',
+    'December 13th, 2022',
+    """I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happened to be. No excuses for not coding from the middle of the field!""",
+    'I am feeling 😁',
+    'You can follow my progress at replit.com/@mvalov',
+    'and of course here: github.com/mivalov/100_days_of_code',
+    sep='\n'
+)

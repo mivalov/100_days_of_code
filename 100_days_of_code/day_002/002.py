fa7eff8886e50ae89e8d0c42d470444c2f13a705
Miroslav Valov <91765698+mivalov@users.noreply.github.com>
2022-12-14 21:16:59 +0100

    Add challenge for day 2

---
+++ 100_days_of_code/day_002/002.py
@@ -0,0 +1,8 @@
+# Day 2: Input and Variables
+
+name = input('Your name is ')
+food = input('Your favourite food is ')
+music = input('Your favourite music is ')
+place = input('You live in the amazing ')
+
+print('You rock, ', name, '!', sep='')

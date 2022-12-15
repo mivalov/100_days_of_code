62bcb0df56550319a58a3502ce52832fe94381d3
Miroslav Valov <91765698+mivalov@users.noreply.github.com>
2022-12-15 22:01:32 +0100

    Add challenge for day 3

---
+++ 100_days_of_code/day_003/003.py
@@ -0,0 +1,17 @@
+# Day 3: Concatenating
+
+food_type = input('Food type: ')
+plant_type = input('Plant type: ')
+cooking_method = input('Method of cooking: ')
+word = input('Word to describe burned food: ')
+item = input('Household item: ')
+
+print(
+  cooking_method,
+  food_type,
+  'with',
+  word,
+  plant_type,
+  'on a bed of',
+  item
+)
